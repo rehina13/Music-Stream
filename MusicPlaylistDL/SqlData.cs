757d03d5955cb94f7e7aab04dcc54ad8807697fe
admin <admin@DESKTOP-JNQ2TIM>
2024-06-28 01:37:47 +0800

    Not yet done

---
+++ MusicPlaylistDL/SqlData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistDL
+{
+    public class User
+    {
+        public string Username { get; set; }
+    }
+
+    public class SqlData
+    {
+        static string connectionString = "Data Source=DESKTOP-JNQ2TIM;Initial Catalog=MusicPlaylistDL;Integrated Security=True;";
+
+        public static void Connect()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                
+            }
+        }
+
+        public static List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string selectStatement = "SELECT Artist, Album and songs FROM users";
+                SqlCommand selectCommand = new SqlCommand(selectStatement, sqlConnection);
+
+                sqlConnection.Open();
+
+                using (SqlDataReader reader = selectCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        users.Add(new User
+                        {
+                            Username = reader["username"].ToString()
+                        });
+                    }
+                }
+            }
+
+            return users;
+        }
+    }
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using MusicPlaylistDL;
-using PlaylistSongBL;
 
 namespace UI
 {
@@ -14,13 +13,15 @@ namespace UI
     {
         static void Main(string[] args)
         {
+            List<User> usersFROMDB = SqlData.GetUsers();
+
             Console.WriteLine("Welcome to Your Music Stream Choice!");
 
-            // User login process
+            // Please enter your username once you login.
             Console.WriteLine("Please enter your username:");
             string username = Console.ReadLine();
 
-            Console.WriteLine("Hello, {username}! Please enter your Artist Choice.");
+            Console.WriteLine($"Hello, {username}! Please enter your Artist Choice.");
 
             MusicInfo musicInfo = new MusicInfo();
 
@@ -28,19 +29,43 @@ namespace UI
             musicInfo.Artist = Console.ReadLine();
 
             Console.WriteLine("\nYour music stream choice is already done.");
-            
+
             Console.WriteLine("\nMusic Information:");
 
+            Dictionary<string, (string Album, string[] Songs)> artistSongs = InitializeArtistSongs();
 
-            // Initialize artist songs
-            Dictionary<string, (string Album, string[] Songs)> artistSongs = ArtistSong.InitializeArtistSongs();
-
-            // Find and display songs by the entered artist
-            PlaylistSong.FindAndDisplaySongs(musicInfo.Artist, artistSongs);
-
-
+            FindAndDisplaySongs(musicInfo.Artist, artistSongs);
 
             Console.ReadLine();
         }
+
+        public static Dictionary<string, (string Album, string[] Songs)> InitializeArtistSongs()
+        {
+            return new Dictionary<string, (string Album, string[] Songs)>
+            {
+                { "Artist1", ("Album1", new string[] { "Song1", "Song2" }) },
+                { "Artist2", ("Album2", new string[] { "Song3", "Song4" }) }
+            };
+        }
+
+        public static void FindAndDisplaySongs(string artist, Dictionary<string, (string Album, string[] Songs)> artistSongs)
+        {
+            if (artistSongs.ContainsKey(artist))
+            {
+                var info = artistSongs[artist];
+                Console.WriteLine($"Artist: {artist}");
+                Console.WriteLine($"Album: {info.Album}");
+                Console.WriteLine("Songs:");
+                foreach (var song in info.Songs)
+                {
+                    Console.WriteLine($"- {song}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Artist not found.");
+            }
+        }
     }
 }
+
